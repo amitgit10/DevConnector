b1cebcb40696402c2afe5b7ffb4b1d07c49cd6bd
Amit <amitgit10>
2023-12-25 15:16:05 +0530

    Connected mongo db

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
+const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
+
+//Connect Database
+connectDB();
 
 app.get("/", (req, res) => res.send("App server is running."));
 
